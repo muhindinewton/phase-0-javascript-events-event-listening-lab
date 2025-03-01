2fa7136cc250cdb35ddb0fcbe284a92ea1c33345
Newton Muhindi <muhindinewton@gmail.com>
2025-03-01 09:45:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Hee heeeh. that tickes!');
+}
+
+input.addEventListener('click', clickAlert);
+
 }
